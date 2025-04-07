b7266aa7e42814d58eac1f792f4423d2414322b3
unknown <hackerviruse04@gmail.com>
2025-04-08 03:02:35 +0530

    .

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server"
 import { db } from "~/server/db"
 
 export async function POST(req: Request){
-    const data = await req.json()
-    console.log('clerk webhook received', JSON.stringify(data, null, 2))
+    const {data} = await req.json()
+    console.log('clerk webhook received', data)
 
     const emailAddress = data.email_addresses?.[0]?.email_address;
     const firstName = data.first_name
